b11c851075d043885e6ae95c45de750c163e2f24
Abrasalas <abraham.salas.reyes@gmail.com>
2017-04-17 21:25:17 -0500

    Segundo commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,22 @@ category3 = Category.create name: 'Category 3'
 Product.create name: 'Product 1', price: 100, category_id: category1.id
 Product.create name: 'Product 2', price: 200, category: category1
 category1.products << Product.create(name: 'Product 3', price:150)
+
+product = category2.products.build(name: 'Product 4', price: 300)
+puts product.inspect
+product.save
+puts product.inspect
+product.destroy
+puts '________'
+pro = Product.last
+pro.premium = true
+pro.save
+puts pro.inspect
+puts '______________'
+
+Category.all.each do |c|
+	c.update name: c.name.upcase
+	puts c.name
+end
+
+
